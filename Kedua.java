d38531c5a9b7c76440f8c85df85f2cca81b37a77
MUHAMMAD ALIF IRSAN <61741175+Aalifmuhammad@users.noreply.github.com>
2020-03-12 19:29:15 +0800

    Add files via upload

---
+++ Kedua.java
@@ -0,0 +1,7 @@
+public class Kedua{
+	
+	public static void main(String[] args) {
+		System.out.print("Belajar Bahasa Pemograman Java");
+	}
+	
+}
---
+++ Ketiga.java
@@ -0,0 +1,7 @@
+public class Ketiga{
+	
+	public static void main(String[] args) {
+		System.out.print("Selesai Belajar Pemograman Java");
+	}
+	
+}
